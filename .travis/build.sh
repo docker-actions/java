44dffa89a1b590602d125259753b4ab6092adac9
Ilja Bobkevic <ilja@spotify.com>
2018-09-07 20:31:13 +0200

    Switch to travis env instead of versions.txt

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,22 +5,16 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for version in 8 10; do
-	echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec java \"\$@\"" > ${version}/entrypoint.sh
-  for params in $(< ${version}/versions.txt); do
-    arr_param=(${params//,/ })
-    build_args=''
-    for build_arg in ${arr_param[@]}; do
-      build_args="${build_args} --build-arg ${build_arg}"
-      eval "export ${build_arg}"
-    done
-
-    image_name=''
-    if [ "x${tag}" = "xlatest" ]; then
-      image_name="${docker_org}/java:${version}-${JAVA_PACKAGE}-${tag}"
-    else
-      image_name="${docker_org}/java:${JAVA_VERSION}-${JAVA_PACKAGE}-${tag}"
-    fi
-    docker build ${build_args} -t ${image_name} ${version}
-  done
+echo -e "#!/usr/bin/env bash\nset -Eeuo pipefail\nexec java \"\$@\"" > ${BUILD_PATH}/entrypoint.sh
+build_args=''
+for ba in $BUILD_ARGS; do
+  build_args="${build_args} --build-arg ${ba}=${!ba}"
 done
+
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/java:${BUILD_PATH}-${JAVA_PACKAGE}-${tag}"
+else
+  image_name="${docker_org}/java:${JAVA_VERSION}-${JAVA_PACKAGE}-${tag}"
+fi
+docker build ${build_args} -t ${image_name} ${BUILD_PATH}
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,19 +5,10 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for version in 8 10; do
-  for params in $(< ${version}/versions.txt); do
-    arr_param=(${params//,/ })
-    for build_arg in ${arr_param[@]}; do
-      eval "export ${build_arg}"
-    done
-
-    image_name=''
-    if [ "x${tag}" = "xlatest" ]; then
-      image_name="${docker_org}/java:${version}-${JAVA_PACKAGE}-${tag}"
-    else
-      image_name="${docker_org}/java:${JAVA_VERSION}-${JAVA_PACKAGE}-${tag}"
-    fi
-    docker push ${image_name}
-  done
-done
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/java:${BUILD_PATH}-${JAVA_PACKAGE}-${tag}"
+else
+  image_name="${docker_org}/java:${JAVA_VERSION}-${JAVA_PACKAGE}-${tag}"
+fi
+docker push ${image_name}
